43666448f96196d97bc1b17051d448140604510f
doolan <doolansr@rose-hulman.edu>
2014-05-01 22:02:15 -0400

    Final Project

--- Distrib.m
+++ Distrib.m
@@ -1,18 +1,20 @@
-function [G] = Distrib(y,pp,stack,damp,num)
+function [G] = Distrib(y,ydel,pp,stack,damp,num)
 %PS Stands for Processing speed, Size is the available space
 % 1 is the core that is sharing its data
 % 2 and 3 are cores receiving data
 % G is the amount to go to each of the boxes
     
 
-amountToReAssign=(stack(num)-(stack(num)-y(num))-.15*stack(num));
+amountToReAssign=(y(num)-.01*stack(num));
 if(amountToReAssign < 0)
    G=[0;0;0];
    
     return;
 end
 
-remains=stack-y;
+remains=stack-ydel(1:3);
+remains(num)=stack(num)-y(num);
+
 k=pp.*remains;
 W=k/(sum(k));
 
@@ -20,5 +22,7 @@ dataChunk=amountToReAssign/(sum(W));
 
 G=W*dataChunk;
 G=damp*G';
+
+
 return;
 end
--- test2.m
+++ test2.m
@@ -1,12 +1,17 @@
-function[] = test1
+function[queue] = test1
 tspan=[0 20];
  sol = ddensd(@ddefun,@dely,@delyp,@history,tspan);
 tn=linspace(0,20);
 [queue,rate]=deval(sol,tn);
 close all
 figure(1)
-plot(tn,queue)
+hold on
+plot(tn,queue(1:3,:))
+%plot(tn,sum(queue(1:3,:),1))
+hold off
 title('queue')
+xlabel('time')
+ylabel('queue size')
 figure(2)
 plot(tn,rate)
 title('rate')
@@ -22,10 +27,10 @@ end
 
 function yp = ddefun(t,y,ydel,ypdel)
 inflow=30;
-pp=[30;40;20];
+pp=[10;20;30];
 stack=[400;400;400];
 
-damping=1;
+damping=.5;
 for i=1:3;
     %each row is what each individual cluster wants
 k(i,:)=Distrib(y,ydel,pp,stack,damping,i);
@@ -40,29 +45,36 @@ end
 %     
         
 k=Matrix(k);
-tp=.1;
+tp=.05;
 
         
 for i=1:3;
     if y(i)/pp(i)>1
         pp(i)=pp(i);
     elseif (y(i)/pp(i))<1 & (y(i)/pp(i))>0
-        pp(i)=pp(i)*(y(i)/pp(i))^.1;
+        pp(i)=pp(i)*(y(i)/pp(i))^.3;
     else
         pp(i)=0;
     end
     
 %k(i,i)=k(i,i)-pp(i);
-   k(i,i)=k(i,i)-(pp(i)-tp*sum(k(k(:,i)>0,i))); 
+ k(i,i)=k(i,i)-(pp(i)-tp*sum(k(k(:,i)>0,i))); 
 end
 
+yp(4,1)=k(1,2);
+yp(5,1)=k(2,3);
+yp(6,1)=k(1,3);
+
+k_old=[pp(1),-ypdel(4,1),-ypdel(6,1);
+        k(1,2),pp(2),-ypdel(5,1);
+        k(1,3),k(2,3),pp(3)];
 
 
+yp(1,1)=inflow-sum(k_old(:,1));
+yp(2,1)=-sum(k_old(:,2));
+yp(3,1)=-sum(k_old(:,3));
 
 
-yp(1,1)=inflow+sum(k(:,1));
-yp(2,1)=sum(k(:,2));
-yp(3,1)=sum(k(:,3));
 
 
 
@@ -73,20 +85,24 @@ yp(3,1)=sum(k(:,3));
 end
 
 function dy = dely(t,y);
-dy(1,1)=t-1;
+dy(1,1)=t-.5;
 %time delay for function
 end
 
 function dyp = delyp(t,y);
-dyp(1,1)=t-.1;
+dyp(1,1)=t-1;
 
 
 
 end
 
 function y = history(t);
-    y(1:3,1)=60;
- 
-   
+    y(1,1)=60;
+    y(2,1)=60;
+    y(3,1)=60;
+    y(4,1)=60;   
+    y(5,1)=60;
+    y(6,1)=60;
+
 end
 
